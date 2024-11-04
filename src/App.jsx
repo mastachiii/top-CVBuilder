96849ac7e8af5c51645984550df73b7ab985eb79
mastachii <mastachii273@gmail.com>
2024-11-04 17:58:37 +0800

    FEAT: Added Education forms.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Preview } from './preview';
 
 function App() {
     const [personalInfo, setPersonalInfo] = useState(info);
+    const [educationInfo, setEducationInfo] = useState([]);
     const personalHandlers = {
         firstName: (e) => setPersonalInfo({ ...personalInfo, firstName: e.target.value }),
         lastName: (e) => setPersonalInfo({ ...personalInfo, lastName: e.target.value }),
@@ -13,10 +14,24 @@ function App() {
         gitHub: (e) => setPersonalInfo({ ...personalInfo, gitHub: e.target.value }),
         linkedIn: (e) => setPersonalInfo({ ...personalInfo, linkedIn: e.target.value }),
     };
+    const educationHandlers = {
+        copy: Array.from(educationInfo),
+        numberOfItems: educationInfo.length,
+        add: () => {
+            educationHandlers.copy.push({
+                address: '',
+                schoolName: '',
+                startDate: '',
+                endDate: '',
+            });
+
+            setEducationInfo(educationHandlers.copy);
+        },
+    };
 
     return (
         <>
-            <Editor personalHandlers={personalHandlers} />
+            <Editor personalHandlers={personalHandlers} educationHandlers={educationHandlers} />
             <Preview personalInfo={personalInfo} />
         </>
     );
--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react';
-
 function Input({ type = 'text', text, onChange }) {
     return <input type={type} placeholder={text} onChange={onChange}></input>;
+}
+
+function Button({ type = 'button', text, onClick }) {
+    return (
+        <button type={type} onClick={onClick}>
+            {text}
+        </button>
+    );
 }
 
 function Personal({ handlers }) {
@@ -17,12 +23,35 @@ function Personal({ handlers }) {
     );
 }
 
-function Editor({ personalHandlers }) {
+function Education({ handlers }) {
     return (
         <div>
+            <Input text='Address' onClick={null} />
+            <Input text='School Name' onClick={null} />
+            <Input text='Start Date' onClick={null} />
+            <Input text='End Date' onClick={null} />
+        </div>
+    );
+}
+
+function Editor({ personalHandlers, educationHandlers }) {
+    const educationForm = (() => {
+        const form = [];
+
+        for (let i = 0; i < educationHandlers.numberOfItems; i++) {
+            form.push(<Education />);
+        }
+
+        return form;
+    })();
+
+    return (
+        <div>
+            <Button text='Add' onClick={educationHandlers.add} />
             <h3>
                 Personal Information: <Personal handlers={personalHandlers} />
             </h3>
+            {educationForm}
         </div>
     );
 }
